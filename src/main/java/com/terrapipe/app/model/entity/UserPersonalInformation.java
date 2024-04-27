3a9cc6ee26694e818b416eee9d8c503ae9a4fe09
Inge.andresCasas <inge.andrescasas@gmail.com>
2024-04-27 14:20:53 -0500

    Update UserPersonalInformation.java

--- src/main/java/com/terrapipe/app/model/entity/UserPersonalInformation.java
+++ src/main/java/com/terrapipe/app/model/entity/UserPersonalInformation.java
@@ -29,10 +29,6 @@ public class UserPersonalInformation {
     private TypeId typeId;
     @ManyToOne(fetch = FetchType.LAZY)
     private Finca finca;    
-    @OneToMany(mappedBy = "userPersonalInformation")
-    @JoinColumn(name = "id_device")
-    private List<Device> dispositivos;
-
 
 
     public UserPersonalInformation() {
