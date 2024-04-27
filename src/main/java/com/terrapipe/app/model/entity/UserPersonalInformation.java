c9d7010fecbcc255f7e5b0d0ea836bc53a5b66a2
Inge.andresCasas <inge.andrescasas@gmail.com>
2024-04-27 14:16:23 -0500

    Update UserPersonalInformation.java

--- src/main/java/com/terrapipe/app/model/entity/UserPersonalInformation.java
+++ src/main/java/com/terrapipe/app/model/entity/UserPersonalInformation.java
@@ -1,14 +1,11 @@
 package com.terrapipe.app.model.entity;
-
 import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
